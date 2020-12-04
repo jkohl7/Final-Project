85df99bf220d566e7b7ec0e5dd109db40f64aeb7
Jacob Kohl <jkohl7@my.wctc.edu>
2020-12-04 14:04:22 -0600

    statrt to add

--- Model/Product.cs
+++ Model/Product.cs
@@ -18,9 +18,9 @@ namespace NorthwindConsole.Model
         public int? CategoryId { get; set; }
         public string QuantityPerUnit { get; set; }
         public decimal? UnitPrice { get; set; }
-        public short? UnitsInStock { get; set; }
-        public short? UnitsOnOrder { get; set; }
-        public short? ReorderLevel { get; set; }
+        public int? UnitsInStock { get; set; }
+        public int? UnitsOnOrder { get; set; }
+        public int? ReorderLevel { get; set; }
         public bool Discontinued { get; set; }
 
         public virtual Category Category { get; set; }
